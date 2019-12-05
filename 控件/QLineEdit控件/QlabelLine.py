00ea4a57b5756146dd05e31eb1997af5d7122e2b
jianbaoshan <1943515313@.com>
2019-12-05 20:56:06 +0800

    QLineEdit控件：表单布局、回显、提示文本、校验器、正则表达式

--- 控件/QLineEdit控件/QlabelLine.py
+++ 控件/QLineEdit控件/QlabelLine.py
@@ -10,6 +10,8 @@ import sys
 #3、Passwd           输入内容以'.'显示
 #4、PasswdEchoEdit   正在输入内容时，正常显示；输入完成之后以'.'显示
 
+#QLineEdit控件限制输入的内容：
+
 
 class QlabelEcho(QWidget):
     def __init__(self):
@@ -17,7 +19,7 @@ class QlabelEcho(QWidget):
         self.setUi()
 
     def setUi(self):
-        self.setWindowTitle('表单回显')
+        self.setWindowTitle('表单回显和校验器')
 
         Normal = QLineEdit()
         NoEcho = QLineEdit()
@@ -36,6 +38,39 @@ class QlabelEcho(QWidget):
         NoEcho.setEchoMode(QLineEdit.NoEcho)                                #设置回显模式：Normal
         Passwd.setEchoMode(QLineEdit.Password)                              #设置回显模式：Normal
         PasswdEchoEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)            #设置回显模式：Normal
+
+        #QLineEdit控件的输入条件
+        intLine = QLineEdit()
+        floatLine = QLineEdit()
+        charLine = QLineEdit()
+
+        form.addRow("整数类型", intLine)             # 设置名字
+        form.addRow("浮点数", floatLine)             # 设置名字
+        form.addRow("数字和字母", charLine)          # 设置名字
+
+        intLine.setPlaceholderText("整数")            #设置提示文本：即在文本显示提示信息
+        floatLine.setPlaceholderText("浮点数")
+        charLine.setPlaceholderText("字母和数字")
+
+        #初始化整数校验器
+        intValid = QIntValidator(self)
+        intValid.setRange(1, 99)                     #设置范围
+
+        #初始化浮点校验器
+        floatValid = QDoubleValidator(self)
+        floatValid.setRange(-360, 360)                                   #设置范围
+        floatValid.setNotation(QDoubleValidator.StandardNotation)
+        floatValid.setDecimals(2)                                        #设置精度，小数点2位
+
+        #初始化字符和数字校验器
+        reg = QRegExp("[a-zA-Z0-9]+$")
+        charValid = QRegExpValidator(self)
+        charValid.setRegExp(reg)
+
+        #设置校验器
+        intLine.setValidator(intValid)
+        floatLine.setValidator(floatValid)
+        charLine.setValidator(charValid)
 
         self.setLayout(form)
 
